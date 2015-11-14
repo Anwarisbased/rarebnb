a2598321afb31d5e2833ed1cbba6adcc98c41dc8
Anwar Khan <anwarwkhan@gmail.com>
2015-11-13 18:25:31 -0700

    still configuring sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'https://mysterious-cove-9183.herokuapp.com/' }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
